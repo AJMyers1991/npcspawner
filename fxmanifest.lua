8712923636b7f853b7724a45773e78934e10fdeb
AJMyers1991 <109767845+AJMyers1991@users.noreply.github.com>
2022-08-04 09:47:40 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+games {"gta5"}
+
+name "npcspawner"
+author "AJMyers1991 - https://github.com/AJMyers1991/"
+version "1.0"
+
+client_scripts {
+    'cl_main.lua',
+    'config.lua'
+}
